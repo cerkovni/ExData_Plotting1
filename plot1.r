b918b2b9639d8be664b8e30b56d221559723918f
cerkovni <Logan.Cerkovnik@gmail.com>
2015-05-10 17:47:32 -0600

    Create plot1.r

---
+++ plot1.r
@@ -0,0 +1,24 @@
+household_power_consumption <- read.csv("C:/Users/Shelby/Downloads/household_power_consumption.txt", sep=";")
+library(dplyr)
+pwrdat <- filter(household_power_consumption, Date == "1/2/2007" | Date == "2/2/2007")
+#mutate(pwrdat, goodtime = paste(as.character(Date), as.character(Time)))
+goodtime = paste(as.character(pwrdat$Date), as.character(pwrdat$Time))
+pwrdat$Time <- goodtime
+pwrdat$Time <- strptime(pwrdat$Time, format = "%d/%m/%Y %H:%M:%S")
+pwrdat$Date <- strftime(pwrdat$Time, format = "%a")
+
+# pwrdat$Global_active_power <- as.double(pwrdat$Global_active_power)
+# pwrdat$Global_reactive_power <- as.double(pwrdat$Global_reactive_power)
+# pwrdat$Voltage <- as.numeric(pwrdat$Voltage)
+# pwrdat$Sub_metering_1 <- as.double(pwrdat$Sub_metering_1)
+# pwrdat$Sub_metering_2 <- as.double(pwrdat$Sub_metering_2)
+# pwrdat$Sub_metering_3 <- as.double(pwrdat$Sub_metering_3)
+
+pwrdat$Global_active_power <- as.numeric(as.character(pwrdat$Global_active_power))
+pwrdat$Global_reactive_power <- as.numeric(as.character(pwrdat$Global_reactive_power))
+pwrdat$Voltage <- as.numeric(as.character(pwrdat$Voltage))
+pwrdat$Sub_metering_1 <- as.numeric(as.character(pwrdat$Sub_metering_1))
+pwrdat$Sub_metering_2 <- as.numeric(as.character(pwrdat$Sub_metering_2))
+pwrdat$Sub_metering_3 <- as.numeric(as.character(pwrdat$Sub_metering_3))
+
+hist(pwrdat$Global_active_power, col = "red", main = "Global Active Power", xlab ="Global Active Power (kilowatts)")
